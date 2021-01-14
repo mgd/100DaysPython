8ea9d62ead4009c36f84fa0af10b0246a41bcc43
Matthew Dawson <Dawsonmattg@gmail.com>
2021-01-14 08:32:02 -0500

    added files from course

--- Day16-OOP/oop-coffee-machine-start/main.py
+++ Day16-OOP/oop-coffee-machine-start/main.py
@@ -1,3 +1,28 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+maker = CoffeeMaker()
+money = MoneyMachine()
+espresso = MenuItem("espresso", 50, 0, 18, 1.50)
+latte = MenuItem("latte", 200, 24, 150, 2.50)
+cappuccino = MenuItem("cappuccino", 250, 24, 100, 3.00)
+menu = Menu()
+
+def getInput():
+    return input(f"What would you like? {menu.get_items()}:").strip()
+
+run = True
+while run:
+
+    choice = getInput()
+    if choice == "off":
+        run = False
+    elif choice == "report":
+        maker.report()
+    else:
+        drink = menu.find_drink(choice)
+        if drink:
+            if maker.is_resource_sufficient(drink):
+                if money.make_payment(drink.cost):
+                    maker.make_coffee(drink)
